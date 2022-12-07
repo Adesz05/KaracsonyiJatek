b97675c8af70077f785211ad204ff6b265ed26c7
Adesz05 <kerekadamtamas@turr.hu>
2022-12-07 10:11:23 +0100

    Mikulas

---
+++ KaracsonyiJatek2/KaracsonyiJatek2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KaracsonyiJatek2
+{
+    public partial class Form1 : Form
+    {
+        static int seged = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seged++;
+            if (seged<25)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
+            }
+            else
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
+            }
+            if (seged>47)
+            {
+                seged = 0;
+                timer1.Stop();
+            }            
+        }
+    }
+}
