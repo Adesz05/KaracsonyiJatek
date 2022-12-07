30a0ef2033d0d177e607aebd57a5ec96f3cdcab6
Adesz05 <kerekadamtamas@turr.hu>
2022-12-07 10:35:12 +0100

    Space-re ugrik

--- KaracsonyiJatek2/KaracsonyiJatek2/Form1.cs
+++ KaracsonyiJatek2/KaracsonyiJatek2/Form1.cs
@@ -12,48 +12,12 @@ namespace KaracsonyiJatek2
 {
     public partial class Form1 : Form
     {
-        static int seged = 0;
-        static int acc = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (acc == 0)
-            {
-                timer1.Start();
-                acc = 15;
-            }
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            pictureBox1.Image = Properties.Resources.ugras;
-            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - acc);
-            acc -= 1;
-
-            if (pictureBox1.Location.Y >= 72)
-            {
-                pictureBox1.Location = new Point(pictureBox1.Location.X, 72);
-                acc = 0;
-                pictureBox1.Image = Properties.Resources.Santa;
-                timer1.Stop();
-            }
-            //if (seged<25)
-            //{
-            //    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 5);
-            //}
-            //else
-            //{
-            //    pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 5);
-            //}
-            //if (seged>47)
-            //{
-            //    seged = 0;
-            //    timer1.Stop();
-            //}            
-        }
+        
     }
 }
---
+++ KaracsonyiJatek2/KaracsonyiJatek2/Jatek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KaracsonyiJatek2
+{
+    public partial class Jatek : Form
+    {
+        static int acc = 0;
+        public Jatek()
+        {
+            InitializeComponent();
+        }
+
+        private void Jatek_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Space)
+            {
+                if (acc == 0)
+                {
+                    timer1.Start();
+                    acc = 15;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.ugras;
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - acc);
+            acc -= 1;
+
+            if (pictureBox1.Location.Y >= 72)
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, 72);
+                acc = 0;
+                pictureBox1.Image = Properties.Resources.Santa;
+                timer1.Stop();
+            }
+        }
+    }
+}
